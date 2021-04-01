1ce936003c2fc485fed94ae6759e308620f90c7c
Yousif Garabet <yousifgarabet@gmail.com>
2021-04-01 19:37:16 +0300

    Basic Routing

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/products', function () {
+    return 'products';
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
