55c98a3dd0d62afa31a4cb9bc3e48ef5d3db06d7
Yousif Garabet <yousifgarabet@gmail.com>
2021-04-02 11:46:58 +0300

    Login User and Get Token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,4 +40,30 @@ class AuthController extends Controller
             'message'=> 'Logged out'
         ];
     }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email'=> 'required|string',
+            'password'=> 'required|string'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message'=> 'Wrong Password'
+            ], 401);
+        }
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 // Protected Routes
 Route::group(['middleware' => 'auth:sanctum'], function() {
